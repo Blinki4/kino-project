92c9dc6005938cacb1bf8f2b8aae1246ad113236
Blinki4 <dmitriy.velg@mail.ru>
2025-04-13 18:31:51 +0300

    Routing added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import Navbar from "./components/Navbar.tsx";
-
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {faBookmark, faFilm, faHouse, faLaptop, faS, faTicket, faUser} from "@fortawesome/free-solid-svg-icons";
+import {Route, Routes} from "react-router-dom";
 
 function App() {
+    library.add(faS, faTicket, faFilm, faBookmark, faLaptop, faUser, faHouse)
 
     return (
         <>
             <Navbar/>
-            <h1>Hello World!</h1>
+            <Routes>
+                <Route path={'/'} element={<>MAIN PAGE</>}/>
+                <Route path={'/films'} element={<>FILMS PAGE</>}/>
+                <Route path={'/series'} element={<>SERIES PAGE</>}/>
+                <Route path={'/my'} element={<>MY PAGE</>}/>
+                <Route path={'/profile'} element={<>PROFILE PAGE</>}/>
+            </Routes>
         </>
     )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,31 @@
 import {FC} from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {Link} from "react-router-dom";
 
 const Navbar: FC = () => {
     return (
         <nav className={'navbar'}>
-            NAVBAR
+            <div className={'navbar__logo-wrapper'}>
+                <FontAwesomeIcon className={'navbar__logo'} icon={["fas", 'ticket']}/>
+            </div>
+            <ul className={'navbar__menu-list'}>
+                <li className={'navbar__menu-item'}>
+                    <Link to={'/'} className={'navbar__menu-link'}>Главная</Link>
+                    <FontAwesomeIcon className={'navbar__logo'} icon={["fas", 'home']}/>
+                </li>
+                <li className={'navbar__menu-item'}>
+                    <Link to={'/films'} className={'navbar__menu-link'}>Фильмы</Link>
+                    <FontAwesomeIcon className={'navbar__logo'} icon={["fas", 'film']}/>
+                </li>
+                <li className={'navbar__menu-item'}>
+                    <Link to={'/series'} className={'navbar__menu-link'}>Сериалы</Link>
+                    <FontAwesomeIcon className={'navbar__logo'} icon={["fas", 'laptop']}/>
+                </li>
+                <li className={'navbar__menu-item'}>
+                    <Link to={'/my'} className={'navbar__menu-link'}>Моё</Link>
+                    <FontAwesomeIcon className={'navbar__logo'} icon={["fas", 'bookmark']}/>
+                </li>
+            </ul>
         </nav>
     );
 };
