6684a2d307556d6d451796bbbb1a658d51fd4fa3
Blinki4 <dmitriy.velg@mail.ru>
2025-04-21 14:28:36 +0300

    navbar routes refactored

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,37 +1,38 @@
 import {FC} from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {Link} from "react-router-dom";
+import {pageConfig} from "../router/pagesConfig.ts";
 
 const Navbar: FC = () => {
     return (
         <nav className={'navbar'}>
             <div className={'navbar__logo-wrapper'}>
-                <Link to={'/'}>
+                <Link to={pageConfig.main}>
                     <FontAwesomeIcon className={'navbar__logo'} icon={["fas", 'clapperboard']}/>
                 </Link>
             </div>
             <div className={'navbar__menu-wrapper'}>
                 <ul className={'navbar__menu-list'}>
                     <li className={'navbar__menu-item'}>
-                        <Link to={'/'} className={'navbar__menu-link'}>
+                        <Link to={pageConfig.main} className={'navbar__menu-link'}>
                             <p className={'navbar__menu-title hidden-mobile'}>Главная</p>
                             <FontAwesomeIcon className={'navbar__menu-icon'} icon={["fas", 'home']}/>
                         </Link>
                     </li>
                     <li className={'navbar__menu-item'}>
-                        <Link to={'/films'} className={'navbar__menu-link'}>
+                        <Link to={pageConfig.films} className={'navbar__menu-link'}>
                             <p className={'navbar__menu-title hidden-mobile'}>Фильмы</p>
                             <FontAwesomeIcon className={'navbar__menu-icon'} icon={["fas", 'film']}/>
                         </Link>
                     </li>
                     <li className={'navbar__menu-item'}>
-                        <Link to={'/series'} className={'navbar__menu-link'}>
+                        <Link to={pageConfig.series} className={'navbar__menu-link'}>
                             <p className={'navbar__menu-title hidden-mobile'}>Сериалы</p>
                             <FontAwesomeIcon className={'navbar__menu-icon'} icon={["fas", 'laptop']}/>
                         </Link>
                     </li>
                     <li className={'navbar__menu-item'}>
-                        <Link to={'/my'} className={'navbar__menu-link'}>
+                        <Link to={pageConfig.my} className={'navbar__menu-link'}>
                             <p className={'navbar__menu-title hidden-mobile'}>Моё</p>
                             <FontAwesomeIcon className={'navbar__menu-icon'} icon={["fas", 'bookmark']}/>
                         </Link>
@@ -43,7 +44,7 @@ const Navbar: FC = () => {
                 </div>
             </div>
             <div className={'navbar__profile-wrapper'}>
-                <Link to={'/profile'}>
+                <Link to={pageConfig.profile}>
                     <FontAwesomeIcon className={'navbar__profile'} icon={["fas", 'user']}/>
                 </Link>
             </div>
