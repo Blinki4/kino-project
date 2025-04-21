7736cf6d57996c69974499f5ab4281b77a053a27
Blinki4 <dmitriy.velg@mail.ru>
2025-04-21 20:10:28 +0300

    refactored

--- src/components/carousel/CarouselItem.tsx
+++ src/components/carousel/CarouselItem.tsx
@@ -1,15 +1,14 @@
 import {FC} from 'react';
-import spiderPoster from "../../assets/spider-man-bg.webp";
 import {IMovie} from "../../types/IMovie.ts";
 
 interface CarouselItemProps {
     movie: IMovie,
 }
 
-const CarouselItem: FC<CarouselItemProps> = () => {
+const CarouselItem: FC<CarouselItemProps> = ({movie}) => {
     return (
         <li className={'carousel__item'}>
-            <img className={'carousel__poster'} src={spiderPoster} alt={''}/>
+            <img className={'carousel__poster'} src={movie.poster} alt={movie.title}/>
         </li>
     );
 };
--- src/components/carousel/carouselService.ts
+++ src/components/carousel/carouselService.ts
@@ -1,7 +1,6 @@
 class CarouselService {
     carouselScroll(direction: string, ref: { current: any }) {
         const scrollOffset: number = 1110
-        console.log(scrollOffset)
         if (direction === 'left') {
             ref.current!.scrollLeft -= scrollOffset;
         }
