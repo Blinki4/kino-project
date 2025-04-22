3f134de613295c1ebc338594fca84e30b2d8f080
Blinki4 <dmitriy.velg@mail.ru>
2025-04-22 14:53:46 +0300

    movie signature added

--- src/pages/MoviePage.tsx
+++ src/pages/MoviePage.tsx
@@ -1,5 +1,6 @@
 import {FC} from "react";
 import {useParams} from "react-router-dom";
+import batmanPoster from "../assets/batman-poster.png";
 
 type MoviePageParams = {
     id: string
@@ -7,10 +8,25 @@ type MoviePageParams = {
 
 const MoviePage: FC = () => {
     const params = useParams<MoviePageParams>()
+    console.log(params.id)
+
+    const movie = {
+        id: 0,
+        title: 'Человек-паук: Через вселенные',
+        year: 2018,
+        poster: batmanPoster,
+        rating: 8.2,
+        ageRating: 18,
+        country: 'USA',
+        movieLength: 183,
+    }
 
     return (
         <div className={'page'}>
-            <h1>{params.id}</h1>
+            <div className={'container'}>
+                <h1>{movie.title}</h1>
+                <img src={movie.poster} alt=""/>
+            </div>
         </div>
     );
 };
